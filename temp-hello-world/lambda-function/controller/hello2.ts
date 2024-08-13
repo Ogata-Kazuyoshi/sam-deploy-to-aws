b83bf2942affa62f75375d51ea002c77dab406da
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-08-13 17:18:29 +0900

    github-ci.ymlでお掃除2

--- temp-hello-world/lambda-function/controller/hello2.ts
+++ temp-hello-world/lambda-function/controller/hello2.ts
@@ -17,7 +17,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: `samconfigの --forceをtrueにしてS 3お掃除.こっちはAPIゲートウェイを追加した ${message}`,
+                message: `github-ci.ymlでお掃除.こっちはAPIゲートウェイを追加した ${message}`,
             }),
         };
     } catch (err) {
