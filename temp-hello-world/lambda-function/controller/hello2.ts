6a30c56cf63d1a866ad7a459fed274028b28858e
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-08-13 16:54:25 +0900

    updateできるかの確認

--- temp-hello-world/lambda-function/controller/hello2.ts
+++ temp-hello-world/lambda-function/controller/hello2.ts
@@ -17,7 +17,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: `AAAAむっちゃIP制限もええ感じ。ええ感じに環境変数切り替えできてます。こっちはAPIゲートウェイを追加した ${message}`,
+                message: `まじでーーー。githubActionsからちゃんとPushトリガーにデプロイでけた.こっちはAPIゲートウェイを追加した ${message}`,
             }),
         };
     } catch (err) {
